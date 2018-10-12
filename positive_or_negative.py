ec3689cb2e2eec53091dade76c5655e5194efd00
tigervanilla <piyushcool102@gmail.com>
2018-10-13 00:22:32 +0530

    Beginner Set-1 Q-1

--- positive_or_negative.py
+++ positive_or_negative.py
@@ -1,9 +1,9 @@
-n=int(input())
-if n>0:
-    print('Positive')
-elif n<0:
-    print('Negative')
-elif n==0:
-    print('Zero')
-else:
+n=input()
+if not n.isdigit():
     print('Invalid Input')
+elif int(n)>0:
+    print('Positive')
+elif int(n)<0:
+    print('Negative')
+else:
+    print('Zero')
