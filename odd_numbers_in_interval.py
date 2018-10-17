ea563879ae0a7a00f6fdb52d9d277a3b0233b98c
tigervanilla <piyushcool102@gmail.com>
2018-10-17 13:08:50 +0530

    Beginner Set-2 Q-14

---
+++ odd_numbers_in_interval.py
@@ -0,0 +1,9 @@
+a,b=(int(i) for i in input().split())
+ans=[]
+if a%2==0:
+    for i in range(a+1,b,2):
+        ans.append(str(i))
+else:
+    for i in range(a+2,b,2):
+        ans.append(str(i))
+print(",".join(ans))
