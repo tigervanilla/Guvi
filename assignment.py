cf1e60693084e73d97d05221541a4859db5ecff6
tigervanilla <piyushcool102@gmail.com>
2018-10-20 22:29:47 +0530

    Beginner Q-83

---
+++ assignment.py
@@ -0,0 +1,12 @@
+while True:
+    try:
+        a,op,b=input().split()
+        a,b=int(a),int(b)
+        if op=='/':
+            ans=a//b
+        elif op=='%':
+            ans=a%b
+        print(ans)
+    except Exception:
+        break
+    
