d50fd75c2d8528a47b5536caea849122953f0954
tigervanilla <piyushcool102@gmail.com>
2018-10-20 09:28:55 +0530

    Beginner Q-52

---
+++ num_to_word.py
@@ -0,0 +1,21 @@
+n=int(input())
+if n==1:
+    print('One')
+elif n==2:
+    print('Two')
+elif n==3:
+    print('Three')
+elif n==4:
+    print('Four')
+elif n==5:
+    print('Five')
+elif n==6:
+    print('Six')
+elif n==7:
+    print('Seeven')
+elif n==8:
+    print('Eight')
+elif n==9:
+    print('Nine')
+elif n==10:
+    print('Ten')
