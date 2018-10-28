4076e96b9d9353c804cfdfa596dcf1cfae15d39d
tigervanilla <piyushcool102@gmail.com>
2018-10-28 20:01:45 +0530

    Beginner Q-66 update

--- prime_again.py
+++ prime_again.py
@@ -1,7 +1,8 @@
-n=int(input())
-isPrime=True
+n=input()
+n=int(n)
+isPrime=1
 for i in range(2,int(n**0.5)+1):
     if n%i==0:
-        isPrime=False
+        isPrime=0
         break
-print('yes') if isPrime else print('no')
+print('yes') if isPrime==1 else print('no')
