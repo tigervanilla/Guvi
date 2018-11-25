f1e5aa76e906ca3c194b1f6df3101bc460f66b0a
tigervanilla <piyushcool102@gmail.com>
2018-11-25 14:15:05 +0530

    player q165

---
+++ player165.cpp
@@ -0,0 +1,25 @@
+// lower_bound and upper_bound in vector 
+#include <iostream> 
+#include <algorithm> // for lower_bound, upper_bound and sort 
+#include <vector>	 // for vector 
+
+using namespace std; 
+
+int main () 
+{ 
+    int n,k;
+    cin>>n>>k;
+    int gfg[n]={0};
+    for(int i=0;i<n;i++)
+        cin>>gfg[i];	
+	vector<int> v(gfg,gfg+n); // 5 6 7 7 6 5 5 6 
+
+	sort (v.begin(), v.end()); // 5 5 5 6 6 6 7 7 
+
+	vector<int>::iterator upper; 
+	upper = upper_bound (v.begin(), v.end(), k); 
+
+	cout << v[(upper - v.begin())] << '\n'; 
+
+	return 0; 
+} 
