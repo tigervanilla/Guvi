4956fbb10bdb55411341874438054c150c6e0f3c
tigervanilla <piyushcool102@gmail.com>
2019-02-17 07:13:17 +0530

    pro 4

--- Pro4.py
+++ Pro4.py
@@ -4,4 +4,4 @@ k = [len(i) for i in givenData]
 for i in range(min(k)):
     if givenData[0][i] != givenData[1][i]:
         cnt += ord(givenData[1][i]) - ord(givenData[0][i])
-print cnt
+print(cnt)
