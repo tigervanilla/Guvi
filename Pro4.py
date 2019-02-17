274606a7907864944999ef4e3f85d997873a5ca9
tigervanilla <piyushcool102@gmail.com>
2019-02-17 07:08:40 +0530

    pro 4

---
+++ Pro4.py
@@ -0,0 +1,7 @@
+givenData = input().split(" ")
+cnt = 0
+k = [len(i) for i in givenData]
+for i in range(min(k)):
+    if givenData[0][i] != givenData[1][i]:
+        cnt += ord(givenData[1][i]) - ord(givenData[0][i])
+print cnt
