308fb4680e60adf80a6fe9c414bf63c54a4f4705
tigervanilla <piyushcool102@gmail.com>
2019-02-17 08:37:59 +0530

    pro 28

---
+++ Pro28.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Hariharan
+ */
+public class Pro28 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int naz = input.nextInt();
+        int[] queueaz = new int[naz];
+
+        for (int iaz = 0; iaz < naz; iaz++)
+            queueaz[iaz] = input.nextInt();
+
+        Arrays.sort(queueaz);
+
+        int waitingTimeaz = 0;
+        int notDisappointedaz = 0;
+
+        for (int iaz = 0; iaz < naz; iaz++) {
+            if (waitingTimeaz <= queueaz[iaz])
+                notDisappointedaz++;
+            waitingTimeaz += queueaz[iaz];
+        }
+        System.out.println(notDisappointedaz);
+        input.close();
+    }
+}
